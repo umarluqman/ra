606340b570e29fa87eee0027501ac1484923492a
sobamisu <derezzed.78@gmail.com>
2021-08-21 12:52:14 +0800

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+
+import App from './App';
+import theme from './theme';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
